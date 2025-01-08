570377a65f3f176106ae732f41036599bac6e194
AchyuthReddyKalagiriri <achyuthreddy0003@gmail.com>
2025-01-08 10:01:00 +0000

    first commit

---
+++ src/components/Track/index.js
@@ -0,0 +1,29 @@
+import {AiOutlineDelete} from 'react-icons/ai'
+import './index.css'
+
+const Track = props => {
+  const {list, deleteTrack} = props
+  const {id, imageUrl, name, genre, duration} = list
+  const onDeleteTrack = () => {
+    deleteTrack(id)
+  }
+  return (
+    <li className="track">
+      <div className="image-name-genre-section">
+        <img src={imageUrl} alt="track" className="image" />
+        <div>
+          <p className="track-name">{name}</p>
+          <p className="genre">{genre}</p>
+        </div>
+      </div>
+      <div className="duration-delete-section">
+        <p className="duration">{duration}</p>
+        <button type="button" className="button" onClick={onDeleteTrack}>
+          <AiOutlineDelete className="delete-icon" />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default Track
